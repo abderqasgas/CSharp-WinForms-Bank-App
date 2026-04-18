4f9b29c1463c77be53d299ed801c925b3b0f6f74
Abdo <abderqasgas@gmail.com>
2026-04-18 16:12:49 -0500

    fixed files not being added

---
+++ Bank-App/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_App
+{
+    public class SavingsAccount:BankAccount
+    {
+        public decimal InterestRate { get; set; }
+
+        public SavingsAccount(string owner, decimal interestRate) : base(owner + $" ({interestRate}%)")
+        {
+            InterestRate = interestRate;
+        }
+
+        public override string Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                return "You cannot deposit $" + amount;
+            if (amount > 20000)
+                return "AML Deposit Limit Reached.";
+
+            decimal interestAmount = (InterestRate / 100) * amount;
+            Balance += amount + interestAmount;
+            return "Deposit completed successfully.";
+        }
+    }
+}
